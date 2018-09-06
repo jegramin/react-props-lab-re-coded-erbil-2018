86c07dffb16d60b262941236c0fd67ba451e7768
Mohammed Ameen <jegroamin@gmail.com>
2018-09-06 07:05:36 +0000

    automatically backed up by learn

--- src/components/Spaceship.js
+++ src/components/Spaceship.js
@@ -1,1 +1,31 @@
 // Code The Spaceship Component Here
+export default class Spaceship extends React.Component{
+  render(){
+    return(
+        <div>
+          <p>{name}</p>
+          <p>{speed}</p>
+          <p>{hasRockets}</p>
+          <p>{colors}</p>
+        </div>
+      )
+  }
+}
+
+
+Spaceship.defaultProps = {
+  speed: 'slow'
+  hasRockets : 'false'
+  colors : ['black', 'red']
+};
+
+
+ReactDOM.render(
+  <Spaceship 
+    name ="Mad Max: Fury Road"
+    speed ="http://image.tmdb.org/t/p/w342/kqjL17yufvn9OVLyXYpvtyrFfak.jpg"
+    hasRockets ={madMaxGenres}
+    colors ="Mad Max: Fury Road"
+  />,
+  document.getElementById('root')
+);
